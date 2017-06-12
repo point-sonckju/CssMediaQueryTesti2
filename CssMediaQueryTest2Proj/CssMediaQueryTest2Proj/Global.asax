95d3eb2eb8b14ed9d31d9cca39cad65a2f7785ea
Juha Sonck <juha.sonck@pointcollege.fi>
2017-06-12 14:38:11 +0300

    Loin uuden MVC-projektin testejä varten

---
+++ CssMediaQueryTest2Proj/CssMediaQueryTest2Proj/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CssMediaQueryTest2Proj.MvcApplication" Language="C#" %>
---
+++ CssMediaQueryTest2Proj/CssMediaQueryTest2Proj/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CssMediaQueryTest2Proj.Startup))]
+namespace CssMediaQueryTest2Proj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
